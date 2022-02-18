1675e7d95c8fdc1fecb890541df73554f5fa651f
cassia-cristina <cassia.cristina.go@gmail.com>
2022-02-18 18:39:13 -0300

    adicionando propriedade headless

--- src/test/java/core/Driver.java
+++ src/test/java/core/Driver.java
@@ -4,16 +4,21 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Driver {
     private static WebDriver driver;
     private static WebDriverWait wait;
+    private static boolean headless;
 
     public Driver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        headless = Boolean.parseBoolean(System.getProperty("headless"));
+        chromeOptions.setHeadless(headless);
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 10);
     }
